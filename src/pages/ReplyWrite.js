cdfbb1a52ff941a732a6398545f11277cfc28a90
aaanjini <zini6633@gmail.com>
2022-02-11 22:45:59 +0900

    답장작성 페이지 작업중

--- src/pages/ReplyWrite.js
+++ src/pages/ReplyWrite.js
@@ -1,21 +1,65 @@
 import React from "react";
 import styled from "styled-components";
-import {Grid, Button, Text, Fixed} from "../elements/index";
+import {Grid, Button, Text, Input} from "../elements/index";
 
 const ReplyWrite = (props) => {
+    const [contents, setContents] = React.useState('');
+    const changeContents = (e) => {
+        setContents(e.target.value);
+    };
+
     return(
         <React.Fragment>
             <Grid>
-                <Grid>
-                    답장쓰기 페이지입니다
-                </Grid>
-                <Grid>
-                    
+
+                <HarfDiv>                
+                    <Text size="25px">답장할 편지📮</Text>
+                    <Grid bg="#eee" padding="20px"  margin="0 20px 0 0" radius="10px">
+                        <Text overflow="scroll">
+                            안녕하세요 편지 내용입니다.
+                            안녕하세요 편지 내용입니다.안녕하세                        안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.
+    요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입                        안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.
+    안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.
+    니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.안녕하세요 편지 내용입니다.
+                        </Text>
+                    </Grid>
+                </HarfDiv>
+
+
+                <HarfDiv>                
+                    <Text size="25px">답장💌</Text>
+                    <Grid>
+                        <Input 
+                            value={contents} 
+                            _onChange={changeContents} 
+                            placeholder="답장을 작성해주세요 :-)" 
+                            multiLine 
+                        />
+                        <label>
+                            <input type="checkbox"/>
+                            익명으로 쓸래요
+                        </label>                   
+                    </Grid>
+                </HarfDiv>
+
+                <Grid padding="20px">
+                    <Button is_fixed>답장 쓰기</Button>
                 </Grid>
             </Grid>
         </React.Fragment>
     );
 
 };
+
+const HarfDiv = styled.div`
+    display: block;
+    width: 100%;
+    @media screen and (min-width: 769px) {
+        display: inline-block;
+        width: calc(50% - 20px);
+        margin: 0 10px
+    };
+`;
+
 
 export default ReplyWrite;
