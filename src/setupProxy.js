79d5a3fc3e6b893ae15fef1cf0d4e37f598a5d28
aaanjini <zini6633@gmail.com>
2022-02-14 21:36:32 +0900

    apis formdata 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/user", {
-      target: "http://15.164.251.132:8080/",
+      target: "http://15.164.251.132:8080",
       changeOrigin: true,
       ws: true,
       pathRewrite: {
