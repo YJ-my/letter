46ae3681a72c7f4f21f3b54534b7904f5b18b7cd
YJ-my <123cjstj@naver.com>
2022-02-15 15:08:22 +0900

    post axios
    post axios

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -24,7 +24,7 @@ const initialState = {
             anonymous: true,
             modifiedAt: "2022-02-15",
             replyCount : 3,
-            uid : "001"
+            // uid : "001"
         },
         {   
             postId: 2,
@@ -33,7 +33,7 @@ const initialState = {
             anonymous: false,
             modifiedAt: "2022-02-15",
             replyCount : 1,
-            uid : "002"
+            // uid : "002"
         },
 
     ]
@@ -47,63 +47,86 @@ const initalPost = {
     modifiedAt:"2202-02-12",
 };
 
+//게시글 조회
 const getPostDB = () => {
-    return async function (dispatch, getState, {history}) {
-        
-        // await postApis.get(`/api/mains`).then((respones)=>{
-        //     //console.log(respones.data);
+    return function (dispatch, getState, {history}) {
+        apis.get(`/api/mains`).then((respones)=>{
+            //console.log(respones.data);
 
-        //     let post_list = [];
+            let post_list = [];
 
-        //     respones.data.post.forEach((_post)=>{
-        //         const post = {
-        //             post_id : _post.postId,
-        //             content: _post.content,
-        //             modifiedAt: _post.modifiedAt,
-        //             nickname: _post.nickname,
-        //             replyCount: _post.replyCount,            
-        //         };
+            respones.data.post.forEach((_post)=>{
+                const post = {
+                    post_id : _post.postId,
+                    content: _post.content,
+                    modifiedAt: _post.modifiedAt,
+                    nickname: _post.nickname,
+                    replyCount: _post.replyCount,            
+                };
 
-        //         post_list.push(post);
-        //     });
-        //     console.log(post_list);
-        //     dispatch(getPost(post_list));
-        // });     
+                post_list.push(post);
+            });
+            console.log(post_list);
+            dispatch(getPost(post_list));
+        });     
     }
 }
 
-const addPostDB = (content, anonymous, uid ) => {
+//게시글 작성,수정
+const addPostDB = (content, anonymous,nickname) => {
     return async function (dispatch, getState, {history}) {
-        //console.log(content, anonymous, uid);
-        // const _user = getState().user.user; 
+        console.log(content, anonymous,nickname);
+        const _user = getState().user.user; 
 
-        // const user_info = {
-        //     nickname: _user.nickname,  // 유저 닉네임
-        //     username: _user.username, //유저 아이디
-        // };
-        // const _post = {
-        //     ...initalPost,
-        //     content:content,
-        //     anonymous:anonymous,
-        //     uid:uid,
-        // }
-        // //console.log(_post)
+        const user_info = {
+            nickname: _user.nickname,  // 유저 닉네임
+            username: _user.username, //유저 아이디
+        };
+        const _post = {
+            ...initalPost,
+            content:content,
+            anonymous:anonymous,
+            nickname:nickname,
+            // uid:uid,
+        }
+        //console.log(_post)
 
-        // const token = getToken();
-        // //console.log(token,"포스트작성 토큰확인");
+        const token = getToken();
+        //console.log(token,"포스트작성 토큰확인");
 
-        // await postApis.post(`/api/posts`, {
-        //     content : _post.content,
-        //     anonymous : _post.anonymous,
-        //     uid : _post.uid
-        // }).then((respones)=>{
-        //     console.log(respones.data,"포스트 성공 데이터");
-        //     window.alert("편지 전달 성공 :)");
-        //     history.push("/");
-        // }).catch((error)=>{
-        //     window.alert("편지 발송에 실패했습니다 :(");
-        //     console.log("포스트 작성 에러",error);
-        // });
+        apis.post(`/api/posts`, {
+            content : _post.content,
+            anonymous : _post.anonymous,
+            nickname: _post.nickname
+            // uid : _post.uid
+        },
+        { headers: {
+            Authorization: `Bearer ${token}`,
+          },}
+        ).then((respones)=>{
+            console.log(respones.data,"포스트 성공 데이터");
+            window.alert("편지 전달 성공 :)");
+            history.push("/");
+        }).catch((error)=>{
+            window.alert("편지 발송에 실패했습니다 :(");
+            console.log("포스트 작성 에러",error);
+        });
+    };
+};
+
+//게시글 삭제
+const deletePostDB = (postId) => {
+    return function (dispatch, getState, {history}){
+        // console.log(postId);
+        const TOKEN = getToken();
+        // console.log(TOKEN)
+        apis.delete(`/api/posts/${postId}`,{postId},{ headers: {
+            "authorization" : `Bearer ${TOKEN}`
+          }, }).then((res) => {
+            console.log(res);
+        }).catch(err => {
+            window.alert("편지 삭제를 실패했습니다 :(");
+        });
     };
 };
 
@@ -129,6 +152,7 @@ export default handleActions ({
 const actionCreators = { //액션 생성자 내보내기
     getPostDB,
     addPostDB,
+    deletePostDB,
 };
 
 export {actionCreators};
