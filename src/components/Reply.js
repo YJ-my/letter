68a36af3c6bab724b09cbad776990093874202e2
aaanjini <zini6633@gmail.com>
2022-02-16 20:50:07 +0900

    답장 연결 && 답장버튼 글쓴이만 노출 성공!

--- src/components/Reply.js
+++ src/components/Reply.js
@@ -1,20 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 import {Grid, Text, Fixed, Button} from "../elements/index";
+import { useDispatch, useSelector } from "react-redux";
 
-const Reply = () => {
+const Reply = (props) => {
+    const dispatch = useDispatch();
+    const postList = useSelector((state) => state.post.list);
+    const user_info = useSelector((state) => state.user.user);
+
+    //console.log(props.username, user_info.username);
+
+    
     return(
         <Grid className="detail_card" min_height="80vh" display="inline-block" bg="#eee" padding="20px"  margin="0 20px 0 0" radius="10px" relative="relative">
             <Grid is_flex>
-                <Text>2022.02.11</Text>
-                <Text bold>닉네임</Text>
+                <Text>{props.localDateTime}</Text>
+                <Text bold>{props.nickname}</Text>
             </Grid>
             <Grid>
-                <Text>어쩌구 저쩌구 답장입니다.</Text>
+                <Text>{props.content}</Text>
             </Grid>
-            <Fixed width="calc(100% - 20px)" left="10px" bottom="10px">
-                <Button>답장 지우기</Button>
-            </Fixed>
+            {props.username === user_info.username? (
+                <Fixed width="calc(100% - 20px)" left="10px" bottom="10px">
+                    <Button>답장 지우기</Button>
+                </Fixed>
+            ) : ""}            
         </Grid>  
     );
 
